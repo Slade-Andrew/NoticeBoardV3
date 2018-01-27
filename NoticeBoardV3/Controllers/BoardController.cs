1581ba4790ec26a3cabe185b84a5fa8e5a459955
Slade-Andrew <slade.andrew75@gmail.com>
2018-01-27 23:35:42 +1300

    Added models and views for Boards and Notices as well as fixed initializer code

---
+++ NoticeBoardV3/Controllers/BoardController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NoticeBoardV3.DAL;
+using NoticeBoardV3.Models;
+
+namespace NoticeBoardV3.Controllers
+{
+    public class BoardController : Controller
+    {
+        private NoticeboardContext db = new NoticeboardContext();
+
+        // GET: Board
+        public ActionResult Index()
+        {
+            var boards = db.Boards.Include(b => b.BoardType).Include(b => b.PerOrg);
+            return View(boards.ToList());
+        }
+
+        // GET: Board/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Board board = db.Boards.Find(id);
+            if (board == null)
+            {
+                return HttpNotFound();
+            }
+            return View(board);
+        }
+
+        // GET: Board/Create
+        public ActionResult Create()
+        {
+            ViewBag.BoardType_ID = new SelectList(db.BoardTypes, "BoardType_ID", "BoardType_Name");
+            ViewBag.PerOrg_ID = new SelectList(db.PerOrgs, "PerOrg_ID", "OrganisationName");
+            return View();
+        }
+
+        // POST: Board/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Board_ID,BoardType_ID,PerOrg_ID,BoardName,BoardDesc")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Boards.Add(board);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BoardType_ID = new SelectList(db.BoardTypes, "BoardType_ID", "BoardType_Name", board.BoardType_ID);
+            ViewBag.PerOrg_ID = new SelectList(db.PerOrgs, "PerOrg_ID", "OrganisationName", board.PerOrg_ID);
+            return View(board);
+        }
+
+        // GET: Board/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Board board = db.Boards.Find(id);
+            if (board == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BoardType_ID = new SelectList(db.BoardTypes, "BoardType_ID", "BoardType_Name", board.BoardType_ID);
+            ViewBag.PerOrg_ID = new SelectList(db.PerOrgs, "PerOrg_ID", "OrganisationName", board.PerOrg_ID);
+            return View(board);
+        }
+
+        // POST: Board/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Board_ID,BoardType_ID,PerOrg_ID,BoardName,BoardDesc")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(board).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BoardType_ID = new SelectList(db.BoardTypes, "BoardType_ID", "BoardType_Name", board.BoardType_ID);
+            ViewBag.PerOrg_ID = new SelectList(db.PerOrgs, "PerOrg_ID", "OrganisationName", board.PerOrg_ID);
+            return View(board);
+        }
+
+        // GET: Board/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Board board = db.Boards.Find(id);
+            if (board == null)
+            {
+                return HttpNotFound();
+            }
+            return View(board);
+        }
+
+        // POST: Board/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Board board = db.Boards.Find(id);
+            db.Boards.Remove(board);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NoticeBoardV3/Controllers/NoticeController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NoticeBoardV3.DAL;
+using NoticeBoardV3.Models;
+
+namespace NoticeBoardV3.Controllers
+{
+    public class NoticeController : Controller
+    {
+        private NoticeboardContext db = new NoticeboardContext();
+
+        // GET: Notice
+        public ActionResult Index()
+        {
+            var notices = db.Notices.Include(n => n.PerOrg);
+            return View(notices.ToList());
+        }
+
+        // GET: Notice/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notice notice = db.Notices.Find(id);
+            if (notice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notice);
+        }
+
+        // GET: Notice/Create
+        public ActionResult Create()
+        {
+            ViewBag.PerOrg_ID = new SelectList(db.PerOrgs, "PerOrg_ID", "OrganisationName");
+            return View();
+        }
+
+        // POST: Notice/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Notice_ID,NoticeName,NoticeDesc,PerOrg_ID")] Notice notice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Notices.Add(notice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PerOrg_ID = new SelectList(db.PerOrgs, "PerOrg_ID", "OrganisationName", notice.PerOrg_ID);
+            return View(notice);
+        }
+
+        // GET: Notice/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notice notice = db.Notices.Find(id);
+            if (notice == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PerOrg_ID = new SelectList(db.PerOrgs, "PerOrg_ID", "OrganisationName", notice.PerOrg_ID);
+            return View(notice);
+        }
+
+        // POST: Notice/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Notice_ID,NoticeName,NoticeDesc,PerOrg_ID")] Notice notice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(notice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PerOrg_ID = new SelectList(db.PerOrgs, "PerOrg_ID", "OrganisationName", notice.PerOrg_ID);
+            return View(notice);
+        }
+
+        // GET: Notice/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notice notice = db.Notices.Find(id);
+            if (notice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notice);
+        }
+
+        // POST: Notice/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Notice notice = db.Notices.Find(id);
+            db.Notices.Remove(notice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
